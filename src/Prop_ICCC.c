990d8ebc6d238bfe03a8c445f116bc97084400bf
Thorsten Otto <admin@tho-otto.de>
2023-03-14 17:12:28 +0100

    Reformat Prop_ICCC.c

--- src/Prop_ICCC.c
+++ src/Prop_ICCC.c
@@ -1,12 +1,13 @@
-//==============================================================================
-//
-// Prop_ICCC.c -- 'Inter Client Communication Convention' related functions.
-//
-// Copyright (C) 2001 Ralph Lowinski <AltF4@freemint.de>
-//------------------------------------------------------------------------------
-// 2001-06-30 - Initial Version.
-//==============================================================================
-//
+/*
+ *==============================================================================
+ *
+ * Prop_ICCC.c -- 'Inter Client Communication Convention' related functions.
+ *
+ * Copyright (C) 2001 Ralph Lowinski <AltF4@freemint.de>
+ *------------------------------------------------------------------------------
+ * 2001-06-30 - Initial Version.
+ *==============================================================================
+ */
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -14,164 +15,217 @@
 #include "pixmap.h"
 #include "wmgr.h"
 #include "ICCC.h"
+#include "x_printf.h"
 
 
-//==============================================================================
-void
-PropDelete (PROPERTIES ** pPool)
+/* ============================================================================== */
+void PropDelete(PROPERTIES **pPool)
 {
-	PROPERTIES * pool = *pPool;
-	
-	if (pool) {
+	PROPERTIES *pool = *pPool;
+
+	if (pool)
+	{
 		int i;
-		for (i = 0; i < XrscPOOLSIZE (pool->Pool); ++i) {
+
+		for (i = 0; i < XrscPOOLSIZE(pool->Pool); ++i)
+		{
 			p_PROPERTY p;
-			while ((p = XrscPOOLITEM (pool->Pool, i))) XrscDelete (pool->Pool, p);
+
+			while ((p = XrscPOOLITEM(pool->Pool, i)))
+				XrscDelete(pool->Pool, p);
 		}
-		if (pool->IconPmap) PmapFree (pool->IconPmap, NULL);
-		if (pool->IconMask) PmapFree (pool->IconMask, NULL);
-		free (pool);
+		if (pool->IconPmap)
+			PmapFree(pool->IconPmap, NULL);
+		if (pool->IconMask)
+			PmapFree(pool->IconMask, NULL);
+		free(pool);
 		*pPool = NULL;
 	}
 }
 
 
-//------------------------------------------------------------------------------
-void
-_Prop_ICCC (WINDOW * wind, PROPERTY * prop, BOOL changed)
+/* ------------------------------------------------------------------------------ */
+void _Prop_ICCC(WINDOW *wind, PROPERTY *prop, BOOL changed)
 {
-	PROPERTIES * pool = wind->Properties;
-	BOOL         iccc = xTrue;
-	
-	#define CONTAINS( p, s, m) (p->Length >= offsetof(s, m) \
-	                                       + sizeof (((s*)0uL)->m))
-	switch (prop->Id) {
-		
-		case XA_WM_COMMAND: if (prop->Type == XA_STRING) {
-			WmgrClntUpdate (ClntFind (wind->Id), prop->Data);
-		}	break;
-		
-		case XA_WM_NAME: if (prop->Type == XA_STRING) {
-			if (changed) {
-				WmgrWindName (wind, prop->Data, xFalse);
+	PROPERTIES *pool = wind->Properties;
+	BOOL iccc = xTrue;
+
+#define CONTAINS( p, s, m) (p->Length >= offsetof(s, m) + sizeof (((s*)0uL)->m))
+	switch (prop->Id)
+	{
+	case XA_WM_COMMAND:
+		if (prop->Type == XA_STRING)
+		{
+			WmgrClntUpdate(ClntFind(wind->Id), prop->Data);
+		}
+		break;
+
+	case XA_WM_NAME:
+		if (prop->Type == XA_STRING)
+		{
+			if (changed)
+			{
+				WmgrWindName(wind, prop->Data, xFalse);
 				pool->WindName = prop->Data;
-		} else {
-				WmgrWindName (wind, "", xFalse);
+			} else
+			{
+				WmgrWindName(wind, "", xFalse);
 				pool->WindName = NULL;
 			}
-		}	break;
-		
-		case XA_WM_ICON_NAME: if (prop->Type == XA_STRING) {
-			if (changed) {
-				WmgrWindName (wind, prop->Data, xTrue);
+		}
+		break;
+
+	case XA_WM_ICON_NAME:
+		if (prop->Type == XA_STRING)
+		{
+			if (changed)
+			{
+				WmgrWindName(wind, prop->Data, xTrue);
 				pool->IconName = prop->Data;
-		} else {
-				WmgrWindName (wind, "", xTrue);
+			} else
+			{
+				WmgrWindName(wind, "", xTrue);
 				pool->IconName = NULL;
 			}
-		}	break;
-		
-		case XA_WM_HINTS: if (prop->Type == XA_WM_HINTS) {
-			WmHints * hints  = (WmHints*)prop->Data;
-			PIXMAP  * pmap   = pool->IconPmap;
-			PIXMAP  * mask   = pool->IconMask;
-			BOOL      notify = xFalse;
-			if (changed && (hints->flags & IconPixmapHint)
-				         && CONTAINS (prop, WmHints, icon_pixmap)) {
-				pmap = PmapFind (hints->icon_pixmap);
-			} else {
+		}
+		break;
+
+	case XA_WM_HINTS:
+		if (prop->Type == XA_WM_HINTS)
+		{
+			WmHints *hints = (WmHints *) prop->Data;
+			PIXMAP *pmap = pool->IconPmap;
+			PIXMAP *mask = pool->IconMask;
+			BOOL notify = xFalse;
+
+			if (changed && (hints->flags & IconPixmapHint) && CONTAINS(prop, WmHints, icon_pixmap))
+			{
+				pmap = PmapFind(hints->icon_pixmap);
+			} else
+			{
 				pmap = NULL;
 			}
-			if (pmap != pool->IconPmap) {
-				if (pool->IconPmap) PmapFree  (pool->IconPmap, NULL);
-				pool->IconPmap    = PmapShare (pmap);
-				notify            = xTrue;
+			if (pmap != pool->IconPmap)
+			{
+				if (pool->IconPmap)
+					PmapFree(pool->IconPmap, NULL);
+				pool->IconPmap = PmapShare(pmap);
+				notify = xTrue;
 			}
-			if (changed && (hints->flags & IconMaskHint)
-				         && CONTAINS (prop, WmHints, icon_mask)) {
-				mask = PmapFind (hints->icon_mask);
-			} else {
+			if (changed && (hints->flags & IconMaskHint) && CONTAINS(prop, WmHints, icon_mask))
+			{
+				mask = PmapFind(hints->icon_mask);
+			} else
+			{
 				mask = NULL;
 			}
-			if (mask != pool->IconMask) {
-				if (pool->IconMask) PmapFree  (pool->IconMask, NULL);
-				pool->IconMask    = PmapShare (mask);
-				notify            = xTrue;
+			if (mask != pool->IconMask)
+			{
+				if (pool->IconMask)
+					PmapFree(pool->IconMask, NULL);
+				pool->IconMask = PmapShare(mask);
+				notify = xTrue;
 			}
-			if (notify) WmgrWindIcon (wind);
-		}	break;
-		
-		case XA_WM_NORMAL_HINTS: if (prop->Type == XA_WM_SIZE_HINTS) {
-			if (changed) {
-				SizeHints * hints  = (SizeHints*)prop->Data;
-				if ((hints->flags & PBaseSize)
-				    && CONTAINS (prop, SizeHints, base_height)) {
+			if (notify)
+				WmgrWindIcon(wind);
+		}
+		break;
+
+	case XA_WM_NORMAL_HINTS:
+		if (prop->Type == XA_WM_SIZE_HINTS)
+		{
+			if (changed)
+			{
+				SizeHints *hints = (SizeHints *) prop->Data;
+
+				if ((hints->flags & PBaseSize) && CONTAINS(prop, SizeHints, base_height))
+				{
 					pool->Base.Size.p_x = hints->base_width;
 					pool->Base.Size.p_y = hints->base_height;
-				} else {
-					pool->Base.valid  = xFalse;
+				} else
+				{
+					pool->Base.valid = xFalse;
 				}
-				if ((hints->flags & PMinSize)
-				    && CONTAINS (prop, SizeHints, min_height)) {
+				if ((hints->flags & PMinSize) && CONTAINS(prop, SizeHints, min_height))
+				{
 					pool->Min.Size.p_x = hints->min_width;
 					pool->Min.Size.p_y = hints->min_height;
-					pool->FixedSize  = xTrue;
-				} else {
-					pool->Min.valid  = xFalse;
-					pool->FixedSize  = xFalse;
+					pool->FixedSize = xTrue;
+				} else
+				{
+					pool->Min.valid = xFalse;
+					pool->FixedSize = xFalse;
 				}
-				if ((hints->flags & PMaxSize)
-				    && CONTAINS (prop, SizeHints, max_height)) {
+				if ((hints->flags & PMaxSize) && CONTAINS(prop, SizeHints, max_height))
+				{
 					pool->Max.Size.p_x = hints->max_width;
 					pool->Max.Size.p_y = hints->max_height;
-				} else {
-					pool->Max. valid = xFalse;
-					pool->FixedSize  = xFalse;
+				} else
+				{
+					pool->Max.valid = xFalse;
+					pool->FixedSize = xFalse;
 				}
-				if ((hints->flags & PResizeInc)
-				    && CONTAINS (prop, SizeHints, inc_height)) {
+				if ((hints->flags & PResizeInc) && CONTAINS(prop, SizeHints, inc_height))
+				{
 					pool->Inc.Step.p_x = hints->inc_width;
 					pool->Inc.Step.p_y = hints->inc_height;
-				} else {
-					pool->Inc.valid  = xFalse;
+				} else
+				{
+					pool->Inc.valid = xFalse;
 				}
-			} else {
+			} else
+			{
 				pool->Base.valid = xFalse;
-				pool->Min.valid  = xFalse;
-				pool->Max.valid  = xFalse;
-				pool->Inc.valid  = xFalse;
+				pool->Min.valid = xFalse;
+				pool->Max.valid = xFalse;
+				pool->Inc.valid = xFalse;
 			}
-		}	break;
-		
-		case WM_PROTOCOLS: if (prop->Type == XA_ATOM) {
+		}
+		break;
+
+	case WM_PROTOCOLS:
+		if (prop->Type == XA_ATOM)
+		{
 			pool->ProtoDelWind = xFalse;
-			if (changed) {
-				Atom * atom = (Atom*)prop->Data;
-				int    num  = prop->Length /4;
-				while (num--) {
-					switch (*atom) {
-						case WM_DELETE_WINDOW: pool->ProtoDelWind = xTrue; break;
-						
-						default: {
-							CLIENT * clnt = ClntFind (wind->Id);
-							PRINT (0,"-_Prop_ICCC(W:%X): unknown WM_PROTOCOL ",
-							        wind->Id);
-							if (AtomValid(*atom)) {
-								x_printf ("'%s'.\n", ATOM_Table[*atom]->Name);
-							} else {
-								x_printf ("A:%lX.\n", *atom);
+			if (changed)
+			{
+				Atom *atom = (Atom *) prop->Data;
+				int num = prop->Length / 4;
+
+				while (num--)
+				{
+					switch (*atom)
+					{
+					case WM_DELETE_WINDOW:
+						pool->ProtoDelWind = xTrue;
+						break;
+
+					default:
+						{
+							CLIENT *clnt = ClntFind(wind->Id);
+
+							PRINT(0, "-_Prop_ICCC(W:%X): unknown WM_PROTOCOL ", wind->Id);
+							if (AtomValid(*atom))
+							{
+								x_printf("'%s'.\n", ATOM_Table[*atom]->Name);
+							} else
+							{
+								x_printf("A:%lX.\n", *atom);
 							}
 						}
 					}
 					atom++;
 				}
 			}
-		}	break;
-		
-		default:
-			iccc = xFalse;
+		}
+		break;
+
+	default:
+		iccc = xFalse;
+		break;
 	}
-	#undef CONTAINS
-	
-	if (changed) prop->ICCC = iccc;
+#undef CONTAINS
+
+	if (changed)
+		prop->ICCC = iccc;
 }
