a8a53d58bb4b268b42dd71a41978745d83970486
Thorsten Otto <admin@tho-otto.de>
2023-03-14 17:39:18 +0100

    Reformat colormap.c

--- src/color_db.c
+++ src/color_db.c
@@ -1,14 +1,16 @@
-//==============================================================================
-//
-// color_db.c -- Assignments of color strings to rgb values.
-//
-// The list must be sorted by the strings to have the binary search working!
-//
-// Copyright (C) 2000,2001 Ralph Lowinski <AltF4@freemint.de>
-//------------------------------------------------------------------------------
-// 2000-12-14 - Module released for beta state.
-// 2000-08-26 - Initial Version.
-//==============================================================================
+/*
+ *==============================================================================
+ *
+ * color_db.c -- Assignments of color strings to rgb values.
+ *
+ * The list must be sorted by the strings to have the binary search working!
+ *
+ * Copyright (C) 2000,2001 Ralph Lowinski <AltF4@freemint.de>
+ *------------------------------------------------------------------------------
+ * 2000-12-14 - Module released for beta state.
+ * 2000-08-26 - Initial Version.
+ *==============================================================================
+ */
 
 ENTRY ("alice blue",             240,248,255),
 ENTRY ("aliceblue",              240,248,255),
