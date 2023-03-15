164aef7f7af2c9af67ed3809978d5a8d5431ed1c
Thorsten Otto <admin@tho-otto.de>
2023-03-15 05:56:35 +0100

    Reformat xrsc.c

--- src/wmgr_prog.c
+++ src/wmgr_prog.c
@@ -1,16 +1,20 @@
-//==============================================================================
-//
-// wmgr_prog.c -- Execution of client programs.
-//
-// Copyright (C) 2001 Ralph Lowinski <AltF4@freemint.de>
-//------------------------------------------------------------------------------
-// 2001-07-10 - Initial Version.
-//==============================================================================
-//
+/*
+ *==============================================================================
+ *
+ * wmgr_prog.c -- Execution of client programs.
+ *
+ * Copyright (C) 2001 Ralph Lowinski <AltF4@freemint.de>
+ *------------------------------------------------------------------------------
+ * 2001-07-10 - Initial Version.
+ *==============================================================================
+ */
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
 #include <dirent.h>
 #include <sys/stat.h>
@@ -21,119 +25,148 @@
 #include "x_mint.h"
 
 
-//------------------------------------------------------------------------------
-static char *
-tos2unix (char * path)
+/* ------------------------------------------------------------------------------ */
+static char *tos2unix(char *path)
 {
-	char * unx = path;
-	
-	if (path[1] == ':') {
-		char drv = toupper (path[0]);
-		int  len = -1;
-		char buf[NAME_MAX+1] = "/";
-		
-		if (drv == 'U') {
+	char *unx = path;
+
+	if (path[1] == ':')
+	{
+		char drv = toupper(path[0]);
+		int len = -1;
+		char buf[NAME_MAX + 1] = "/";
+
+		if (drv == 'U')
+		{
 			len = 0;
-		
-		} else if (drv >= 'A'  &&  drv <= 'Z') {
-			int         dev = drv - 'A';
+		} else if (drv >= 'A' && drv <= 'Z')
+		{
+			int dev = drv - 'A';
 			struct stat s;
+
 			buf[1] = dev + 'a';
 			buf[2] = '\0';
-		
-			if (!stat (buf, &s) &&  s.st_dev == dev) {
+
+			if (stat(buf, &s) == 0 && s.st_dev == dev)
+			{
 				len = 2;
-		
-			} else {
-				DIR * dir = opendir ("/");
-				if (dir) {
-					struct dirent * ent;
-					while ((ent = readdir (dir))) {
-						int  l = ent->d_namlen +1;
-						memcpy (buf +1, ent->d_name, l);
-						if (!stat (buf, &s) &&  s.st_dev == dev) {
+			} else
+			{
+				DIR *dir = opendir("/");
+
+				if (dir)
+				{
+					struct dirent *ent;
+
+					while ((ent = readdir(dir)))
+					{
+						int l = ent->d_namlen + 1;
+
+						memcpy(buf + 1, ent->d_name, l);
+						if (!stat(buf, &s) && s.st_dev == dev)
+						{
 							len = l;
 							break;
 						}
 					}
-					closedir (dir);
+					closedir(dir);
 				}
 			}
 		}
-		if (len >= 0  && (unx = malloc (len + strlen (path += 2)))) {
-			char * u, * p;
-			if (len) u = (char*)memcpy (unx, buf, len) + len;
-			else     u = unx;
+		if (len >= 0 && (unx = malloc(len + strlen(path += 2))) != NULL)
+		{
+			char *u;
+			char *p;
+
+			if (len)
+				u = (char *) memcpy(unx, buf, len) + len;
+			else
+				u = unx;
 			*(u++) = '/';
-			while ((p = strchr (++path, '\\'))) {
+			while ((p = strchr(++path, '\\')) != NULL)
+			{
 				int l = p - path;
-				u      = (char*)memcpy (u, path, l) + l;
+
+				u = (char *) memcpy(u, path, l) + l;
 				*(u++) = '/';
-				path   = p;
+				path = p;
 			}
-			strcpy (u, path);
+			strcpy(u, path);
 		}
 	}
 	return unx;
 }
 
-//==============================================================================
-short
-WmgrLaunch (const char * prg, int argc, const char * arg_v[])
+/* ============================================================================== */
+short WmgrLaunch(const char *prg, int argc, const char *arg_v[])
 {
-	extern char ** environ;
-	char        ** src = environ;
-	short          pid = -1;
-	const char   * cmd = "\x7F\0";
-	char         * env, * unx;
-	size_t         len,   u_l;
-	int            i;
-	
-	char * argv[argc];
-	
-	if (!(unx = tos2unix ((char*)prg))) {
+	char **src = environ;
+	short pid = -1;
+	const char *cmd = "\x7F\0";
+	char *env;
+	char *unx;
+	size_t len;
+	size_t u_l;
+	int i;
+	char *argv[argc];
+
+	if ((unx = tos2unix((char *) prg)) == NULL)
+	{
 		return -1;
 	}
-	
-	u_l = strlen (unx) +1;
-	len = u_l +6 +1;
-	for (i = 0; src[i]; i++) {
-		if (!strncmp (src[i], "ARGV=", 5)) break;
-		len += strlen (src[i]) +1;
+
+	u_l = strlen(unx) + 1;
+	len = u_l + 6 + 1;
+	for (i = 0; src[i]; i++)
+	{
+		if (strncmp(src[i], "ARGV=", 5) == 0)
+			break;
+		len += strlen(src[i]) + 1;
 	}
-	for (i = 0; i < argc; i++) {
-		if (!(argv[i] = tos2unix ((char*)arg_v[i]))) {
+	for (i = 0; i < argc; i++)
+	{
+		if ((argv[i] = tos2unix((char *) arg_v[i])) == NULL)
+		{
 			argc = i;
 			break;
 		}
-		len += strlen (argv[i]) +1;
+		len += strlen(argv[i]) + 1;
 	}
-	if ((env = malloc (len))) {
-		char * e = env;
-		for (i = 0; src[i]; i++) {
-			size_t l = strlen (src[i]) +1;
-			memcpy (e, src[i], l);
+	if ((env = malloc(len)) != NULL)
+	{
+		char *e = env;
+
+		for (i = 0; src[i]; i++)
+		{
+			size_t l = strlen(src[i]) + 1;
+
+			memcpy(e, src[i], l);
 			e += l;
 		}
-		memcpy (e, "ARGV=", 6);
+		memcpy(e, "ARGV=", 6);
 		e += 6;
-		memcpy (e, unx, u_l);
+		memcpy(e, unx, u_l);
 		e += u_l;
-		for (i = 0; i < argc; i++) {
-			size_t l = strlen (argv[i]) +1;
-			memcpy (e, argv[i], l);
+		for (i = 0; i < argc; i++)
+		{
+			size_t l = strlen(argv[i]) + 1;
+
+			memcpy(e, argv[i], l);
 			e += l;
 		}
 		*e = '\0';
-		pid = Pexec (100, prg, cmd, env);
-		
-		free (env);
+		pid = Pexec(100, prg, cmd, env);
+
+		free(env);
 	}
-	
-	for (i = 0; i < argc; i++) {
-		if (argv[i] != arg_v[i]) free (argv[i]);
+
+	for (i = 0; i < argc; i++)
+	{
+		if (argv[i] != arg_v[i])
+			free(argv[i]);
 	}
-	if (unx != prg) free (unx);
-	
+	if (unx != prg)
+		free(unx);
+
 	return pid;
 }
--- src/xrsc.c
+++ src/xrsc.c
@@ -1,21 +1,22 @@
-//==============================================================================
-//
-// xrsc.c
-//
-// Copyright (C) 2000,2001 Ralph Lowinski <AltF4@freemint.de>
-//------------------------------------------------------------------------------
-// 2000-12-14 - Module released for beta state.
-// 2000-10-02 - Initial Version.
-//==============================================================================
-//
+/*
+ *==============================================================================
+ *
+ * xrsc.c
+ *
+ * Copyright (C) 2000,2001 Ralph Lowinski <AltF4@freemint.de>
+ *------------------------------------------------------------------------------
+ * 2000-12-14 - Module released for beta state.
+ * 2000-10-02 - Initial Version.
+ *==============================================================================
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <string.h>
 #include <X11/Xmd.h>
 
-#include "xrsc.h"
 #include "x_printf.h"
+#include "xrsc.h"
 
 #include <X11/Xproto.h>
 
@@ -24,14 +25,18 @@ BOOL DBG_XRSC_TypeError = xFalse;
 
 
 #if 0
-# define HEAVY_DEBUG
+#define HEAVY_DEBUG
 
-static CARD32 DEBUG_SUM (p_XRSCPOOL pool)
+static CARD32 DEBUG_SUM(p_XRSCPOOL pool)
 {
 	CARD32 sum = pool->mask_bits;
-	int    i   = sum;
-	do { sum += (CARD32)pool->item[i]; } while (i--);
-	
+	int i = sum;
+
+	do
+	{
+		sum += (CARD32) pool->item[i];
+	} while (i--);
+
 	return sum;
 }
 
@@ -41,24 +46,28 @@ static CARD32 DEBUG_SUM (p_XRSCPOOL pool)
 		exit(1); \
 	}
 
-static char __mem[1024*1024*2], * __ptr = __mem;
+static char __mem[1024 * 1024 * 2];
+static char *__ptr = __mem;
 
-static void * _Xrsc_malloc (size_t size)
+static void *_Xrsc_malloc(size_t size)
 {
-	void * m = (void*)__ptr;
-	__ptr += (size +3) & ~3uL;
-	
+	void *m = (void *) __ptr;
+
+	__ptr += (size + 3) & ~3uL;
+
 	return m;
 }
+
 #define malloc(s) _Xrsc_malloc(s)
 #define free(m)   {}
 
-void _Xrsc_Free (void * m);
-void _Xrsc_Free (void * m)
+void _Xrsc_Free(void *m);
+void _Xrsc_Free(void *m)
 {
-	char * p = m;
-	
-	if (p >= __mem  &&  p < __mem + sizeof(__mem)) {
+	char *p = m;
+
+	if (p >= __mem && p < __mem + sizeof(__mem))
+	{
 		x_printf("\n\nPLONK %lX\n", p - __mem);
 		exit(1);
 	}
@@ -68,147 +77,151 @@ void _Xrsc_Free (void * m)
 	pool->DEBUG_chk = DEBUG_SUM(pool)
 
 #else
-# define DEBUG_CHK(pool,func,id)
-# define DEBUG_SET(pool)
+#define DEBUG_CHK(pool,func,id)
+#define DEBUG_SET(pool)
 #endif
 
 
-//==============================================================================
-void
-_xrsc_pool_init (p_XRSCPOOL pool, size_t size)
+/* ============================================================================== */
+void _xrsc_pool_init(p_XRSCPOOL pool, size_t size)
 {
 	size -= sizeof(struct s_XRSCPOOL) - sizeof(p_XRSC);
-	
+
 #ifdef HEAVY_DEBUG
-	if (size & 3) {
+	if (size & 3)
+	{
 		x_printf("\nsize = %li & 3\n", size);
 		exit(1);
 	}
 #endif
-	
-	memset (&pool->item, 0, size);
-	pool->mask_bits = (size /4) -1;
-	DEBUG_SET (pool);
+
+	memset(&pool->item, 0, size);
+	pool->mask_bits = (size / 4) - 1;
+	DEBUG_SET(pool);
 }
 
 
-//==============================================================================
-void
-_xrsc_insert (p_XRSCPOOL pool, p_XRSC r)
+/* ============================================================================== */
+void _xrsc_insert(p_XRSCPOOL pool, p_XRSC r)
 {
-	p_XRSC * p = & pool->item[r->Id & pool->mask_bits];
-	
-	DEBUG_CHK (pool, _xrsc_insert, r->Id);
-	
+	p_XRSC *p = &pool->item[r->Id & pool->mask_bits];
+
+	DEBUG_CHK(pool, _xrsc_insert, r->Id);
+
 #ifdef HEAVY_DEBUG
-	if (r->DEBUG_tag) {
-		x_printf ("### _xrsc_insert(%p,Id:%X) tag not cleared!\n\n", pool, r->Id);
+	if (r->DEBUG_tag)
+	{
+		x_printf("### _xrsc_insert(%p,Id:%X) tag not cleared!\n\n", pool, r->Id);
 		exit(1);
 	}
 	r->DEBUG_tag = pool;
 #endif
-	
+
 	r->NextXRSC = *p;
-	*p          = r;
-	
-	DEBUG_SET (pool);
+	*p = r;
+
+	DEBUG_SET(pool);
 }
 
 
-//==============================================================================
-p_XRSC
-_xrsc_create (p_XRSCPOOL pool, CARD32 id, size_t size)
+/* ============================================================================== */
+p_XRSC _xrsc_create(p_XRSCPOOL pool, CARD32 id, size_t size)
 {
-	p_XRSC r = malloc (size);
-	if (r) {
+	p_XRSC r = malloc(size);
+
+	if (r)
+	{
 		r->NextXRSC = NULL;
-		r->Id       = id;
-		// the flag bitfield stays undefined!
-		
-		_xrsc_insert (pool, r);
-	
-	} else {
-		x_printf ("_xrsc_create(%lX,%li): memory exhaustetd.\n", id, size);
+		r->Id = id;
+		/* the flag bitfield stays undefined! */
+
+		_xrsc_insert(pool, r);
+	} else
+	{
+		x_printf("_xrsc_create(%lX,%li): memory exhausted.\n", id, size);
 	}
 	return r;
 }
 
 
-//==============================================================================
-BOOL
-_xrsc_remove (p_XRSCPOOL pool, p_XRSC r)
+/* ============================================================================== */
+BOOL _xrsc_remove(p_XRSCPOOL pool, p_XRSC r)
 {
-	p_XRSC * p = & pool->item[r->Id & pool->mask_bits];
-	
-	DEBUG_CHK (pool, _xrsc_remove, r->Id);
-	
+	p_XRSC *p = &pool->item[r->Id & pool->mask_bits];
+
+	DEBUG_CHK(pool, _xrsc_remove, r->Id);
+
 #ifdef HEAVY_DEBUG
-	if (!r->DEBUG_tag) {
-		x_printf ("### _xrsc_remove(%p,Id:%X) tag is cleared!\n\n", pool, r->Id);
+	if (!r->DEBUG_tag)
+	{
+		x_printf("### _xrsc_remove(%p,Id:%X) tag is cleared!\n\n", pool, r->Id);
 		exit(1);
 	}
 #endif
 
-	while (*p) {
+	while (*p)
+	{
 #ifdef HEAVY_DEBUG
-		if ((*p)->DEBUG_tag != pool) {
-			x_printf ("### _xrsc_remove(%p,Id:%X) invalid tag %p in Id:%X!\n\n",
-			         pool, r->Id, (*p)->DEBUG_tag, (*p)->Id);
+		if ((*p)->DEBUG_tag != pool)
+		{
+			x_printf("### _xrsc_remove(%p,Id:%X) invalid tag %p in Id:%X!\n\n", pool, r->Id, (*p)->DEBUG_tag, (*p)->Id);
 			exit(1);
 		}
 #endif
 
-		if (*p == r) {
-			*p          = r->NextXRSC;
+		if (*p == r)
+		{
+			*p = r->NextXRSC;
 			r->NextXRSC = NULL;
 #ifdef HEAVY_DEBUG
 			r->DEBUG_tag = NULL;
 #endif
-			
-			DEBUG_SET (pool);
-			
+
+			DEBUG_SET(pool);
+
 			return xTrue;
 		}
 		p = &(*p)->NextXRSC;
 	}
 #ifdef HEAVY_DEBUG
-	x_printf ("### _xrsc_remove(%p,Id:%X) not in pool!\n\n", pool, r->Id);
+	x_printf("### _xrsc_remove(%p,Id:%X) not in pool!\n\n", pool, r->Id);
 	exit(1);
 #endif
-	
+
 	return xFalse;
 }
 
 
-//==============================================================================
-BOOL
-_xrsc_delete (p_XRSCPOOL pool, p_XRSC r)
+/* ============================================================================== */
+BOOL _xrsc_delete(p_XRSCPOOL pool, p_XRSC r)
 {
-	BOOL ok = _xrsc_remove (pool, r);
-	
-	if (ok) free (r);
-	
+	BOOL ok = _xrsc_remove(pool, r);
+
+	if (ok)
+		free(r);
+
 	return ok;
 }
 
 
-//==============================================================================
-p_XRSC
-_xrsc_search (const struct s_XRSCPOOL * pool, CARD32 id)
+/* ============================================================================== */
+p_XRSC _xrsc_search(const struct s_XRSCPOOL *pool, CARD32 id)
 {
 	p_XRSC r = pool->item[id & pool->mask_bits];
-	
-	DEBUG_CHK (pool, _xrsc_search, (unsigned int)id);
-	
-	while (r) {
+
+	DEBUG_CHK(pool, _xrsc_search, (unsigned int) id);
+
+	while (r)
+	{
 #ifdef HEAVY_DEBUG
-		if (r->DEBUG_tag != pool) {
-			x_printf ("### _xrsc_search(%p,Id:%lX) invalid tag %p in Id:%X!\n\n",
-			         pool, id, r->DEBUG_tag, r->Id);
+		if (r->DEBUG_tag != pool)
+		{
+			x_printf("### _xrsc_search(%p,Id:%lX) invalid tag %p in Id:%X!\n\n", pool, id, r->DEBUG_tag, r->Id);
 			exit(1);
 		}
 #endif
-		if (r->Id == id) break;
+		if (r->Id == id)
+			break;
 		r = r->NextXRSC;
 	}
 	return r;
